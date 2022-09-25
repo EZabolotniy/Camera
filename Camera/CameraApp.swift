65de4e4309acbf34e12417797c93e78466ded08d
Evgeniy Zabolotniy <eugenezabolotniy@yandex.ru>
2022-09-25 12:58:05 +0300

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Evgeniy Zabolotniy on 25.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
